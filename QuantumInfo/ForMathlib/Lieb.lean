9ecea7c58f97e60447093ba2d04bef69e9296483
Kevin Zhao <kevinzhao45@outlook.com>
2026-03-06 15:22:11 +0000

    rectified symbol usages

--- QuantumInfo/ForMathlib/Lieb.lean
+++ QuantumInfo/ForMathlib/Lieb.lean
@@ -4,6 +4,7 @@ Released under MIT license as described in the file LICENSE.
 Authors: Alex Meiburg
 -/
 import QuantumInfo.ForMathlib.HermitianMat
+open scoped InnerProductSpace RealInnerProductSpace
 
 /-! Lieb's Inequality .. todo -/
 
@@ -15,6 +16,6 @@ open Classical
 
 theorem LiebConcavity (K : Matrix n m ℂ) (hq : 0 ≤ q) (hr : 0 ≤ r) (hqr : q + r ≤ 1) :
   let F : (HermitianMat m ℂ × HermitianMat n ℂ) → ℝ :=
-      fun (x,y) ↦ ((x ^ q).conj K).inner (y ^ r);
+      fun (x,y) ↦ ⟪((x ^ q).conj K), (y ^ r)⟫;
     ConcaveOn ℝ .univ F := by
   sorry
